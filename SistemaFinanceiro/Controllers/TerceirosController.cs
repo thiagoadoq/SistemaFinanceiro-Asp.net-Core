799a9fd8f899ecc0905761ca10a41c66cbc5c648
thiago alves de oliveira <thiagoadoq@gmail.com>
2019-11-14 22:45:18 -0300

    Criando grid de  terceiros

--- SistemaFinanceiro/Controllers/TerceirosController.cs
+++ SistemaFinanceiro/Controllers/TerceirosController.cs
@@ -26,6 +26,13 @@ namespace SistemaFinanceiro.Controllers
             return View(_Context.Terceiros.ToList());
         }
 
+        //Modelo de grids
+        public IActionResult IndexGrid(string search)
+        {
+            return PartialView(string.IsNullOrWhiteSpace(search) ? _Context.Terceiros.AsQueryable() : _Context.Terceiros.Where(c =>
+                c.Nome.Contains(search) || c.Cpf.Contains(search) || c.Endereco.Contains(search)));
+
+        }
 
         // GET: Terceiros/Edit/5
         public ActionResult Edit(int? id)
